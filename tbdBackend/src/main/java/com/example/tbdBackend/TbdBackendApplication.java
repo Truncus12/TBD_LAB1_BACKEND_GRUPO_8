c3921d5a4d39ca6bb70d0252c2899eb2d600b879
Javier <javier.sanhueza.a@usach.cl>
2023-09-24 18:04:52 -0300

    estructura proyecto

---
+++ tbdBackend/src/main/java/com/example/tbdBackend/TbdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tbdBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbdBackendApplication.class, args);
+	}
+
+}
---
+++ tbdBackend/src/test/java/com/example/tbdBackend/TbdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tbdBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
