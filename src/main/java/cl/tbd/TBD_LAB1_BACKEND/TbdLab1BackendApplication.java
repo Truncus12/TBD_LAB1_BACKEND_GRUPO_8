fc4a2f7938209a32a9c32841104ce31ada78ab53
John Fernandez Cubillos <jjifernandezcubillos@gmail.com>
2023-10-17 10:52:43 -0300

    Agregado CORS al sistema

--- src/main/java/cl/tbd/TBD_LAB1_BACKEND/TbdLab1BackendApplication.java
+++ src/main/java/cl/tbd/TBD_LAB1_BACKEND/TbdLab1BackendApplication.java
@@ -2,6 +2,9 @@ package cl.tbd.TBD_LAB1_BACKEND;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TbdLab1BackendApplication {
@@ -9,4 +12,19 @@ public class TbdLab1BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(TbdLab1BackendApplication.class, args);
     }
+    
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+					.addMapping("/**")
+					.allowedOriginPatterns("*")
+                    .allowCredentials(true)
+					.allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+			}
+		};
+	}
 }
