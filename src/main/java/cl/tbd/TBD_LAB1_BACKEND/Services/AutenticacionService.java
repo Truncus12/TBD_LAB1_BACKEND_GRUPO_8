f26f0e0c1a04cd240560f15ff8a2ea9102c54d19
John Fernandez Cubillos <jjifernandezcubillos@gmail.com>
2023-10-18 20:21:38 -0300

    Eliminado código de sobra en autenticación

--- src/main/java/cl/tbd/TBD_LAB1_BACKEND/Services/AutenticacionService.java
+++ src/main/java/cl/tbd/TBD_LAB1_BACKEND/Services/AutenticacionService.java
@@ -1,18 +1,15 @@
 package cl.tbd.TBD_LAB1_BACKEND.Services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
 
 
 @Service
 public class AutenticacionService {
-    @Autowired
-    private JwtDecoder decodificador;
-
     public int getIdVoluntario(String token){
-        return Integer.parseInt(
-            decodificador.decode(token).getClaimAsString("id_voluntario")
-        );
+        return JWT
+            .decode(token)
+            .getClaim("id_voluntario").asInt();
     }
 }
