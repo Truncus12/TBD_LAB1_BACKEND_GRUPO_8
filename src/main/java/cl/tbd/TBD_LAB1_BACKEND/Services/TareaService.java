1daf442a2246e6ad6d26e8c701b3813e8de950cd
Javier <javier.sanhueza.a@usach.cl>
2023-11-19 12:00:25 -0300

    guardar geom de tarea

--- src/main/java/cl/tbd/TBD_LAB1_BACKEND/Services/TareaService.java
+++ src/main/java/cl/tbd/TBD_LAB1_BACKEND/Services/TareaService.java
@@ -2,6 +2,7 @@ package cl.tbd.TBD_LAB1_BACKEND.Services;
 
 import java.util.List;
 
+import cl.tbd.TBD_LAB1_BACKEND.Entities.TareaEntity;
 import org.postgis.PGgeometry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -33,5 +34,21 @@ public class TareaService {
 
     public DTOTareaVista cancelar(long id, int id_usuario){
         return tareas.cancelar(id, id_usuario);
+    }
+
+    public int insertarTarea(DTOTareaVista tarea) {
+        TareaEntity tareaEntity = new TareaEntity(
+            tarea.nombre,
+            tarea.descripcion,
+            tarea.cant_vol_requeridos,
+            tarea.cant_vol_inscritos,
+            tarea.fecha_inicio,
+            tarea.fecha_fin,
+            tarea.estado_actual,
+            tarea.id_emergencia,
+            tarea.longitud,
+            tarea.latitud
+        );
+        return tareas.insertarTarea(tareaEntity);
     }
 }
